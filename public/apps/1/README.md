f216e05b426f11193ebcfbd4d268c8ac7d1ab965
Princiya <princiya.marina@gmail.com>
2017-08-27 14:24:49 +0200

    selfie app updated

--- public/apps/1/README.md
+++ public/apps/1/README.md
@@ -1,3 +1,9 @@
 ## 1 - Selfie sharing
 
 This is a simple app to click selfies and share with friends using WebRTC.
+
+## Exercise
+
+- Implement different CSS filters
+  - A complete list of CSS filters to be applied can be found [here](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/filter).
+- Implement filters on the streaming video and then capture the selfie
---
+++ public/apps/1/js/signalingServer.js
@@ -0,0 +1,59 @@
+/****************************************************************************
+* Signaling server
+****************************************************************************/
+
+// Connect to the signaling server
+var socket = io.connect();
+
+socket.on('ipaddr', function(ipaddr) {
+  console.log('Server IP address is: ' + ipaddr);
+  // updateRoomURL(ipaddr);
+});
+
+socket.on('created', function(room, clientId) {
+  console.log('Created room', room, '- my client ID is', clientId);
+  isInitiator = true;
+  grabWebCamVideo();
+});
+
+socket.on('joined', function(room, clientId) {
+  console.log('This peer has joined room', room, 'with client ID', clientId);
+  isInitiator = false;
+  createPeerConnection(isInitiator, configuration);
+  grabWebCamVideo();
+});
+
+socket.on('full', function(room) {
+  alert('Room ' + room + ' is full. We will create a new room for you.');
+  window.location.hash = '';
+  window.location.reload();
+});
+
+socket.on('ready', function() {
+  console.log('Socket is ready');
+  createPeerConnection(isInitiator, configuration);
+});
+
+socket.on('log', function(array) {
+  console.log.apply(console, array);
+});
+
+socket.on('message', function(message) {
+  console.log('Client received message:', message);
+  signalingMessageCallback(message);
+});
+
+// Join a room
+socket.emit('create or join', room);
+
+if (location.hostname.match(/localhost|127\.0\.0/)) {
+  socket.emit('ipaddr');
+}
+
+/**
+* Send message to signaling server
+*/
+function sendMessage(message) {
+  console.log('Client sending message: ', message);
+  socket.emit('message', message);
+}
